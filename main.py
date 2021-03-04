26a84ee73caf5f6cce681803d86edc9b398efe3c
Jason Jenkins <jdj0891@yahoo.com>
2021-03-04 08:54:56 -0800

    Fixed sql injection

--- main.py
+++ main.py
@@ -15,40 +15,44 @@ def home():
 
         transaction = "INSERT INTO messages VALUES ('{}', '{}')".format(
             request.remote_addr,
-            request.form['content'],
+            request.form['content'].replace("'", ""),
         )
+
+        print(transaction)
+
+        # print(transaction)
         c.execute(transaction)
         conn.commit()
 
 
     body = """
-<html>
-<body>
-<h1>Messages</h1>
-<h2>Enter a Message</h2>
-<form method="POST">
-    <label for="content">Message</label>
-    <input type="text" name="content"><br>
+    <html>
+    <body>
+    <h1>Messages</h1>
+    <h2>Enter a Message</h2>
+    <form method="POST">
+        <label for="content">Message</label>
+        <input type="text" name="content"><br>
 
-    <input type="submit" value="Submit">
-</form>
+        <input type="submit" value="Submit">
+    </form>
 
-<h2>Messages</h2>
-"""
-    
+    <h2>Messages</h2>
+    """
+
     for m in c.execute("SELECT * FROM messages"):
         body += """
-<div class="message">
-{}: {}
-</div>
-""".format(m[0], m[1])
+        <div class="message">
+        {}: {}
+        </div>
+        """.format(m[0], m[1])
 
     c.close()
 
-    return body 
+    return body
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6779))
+    port = int(os.environ.get("PORT", 6770))
     app.run(host='0.0.0.0', port=port)
 
